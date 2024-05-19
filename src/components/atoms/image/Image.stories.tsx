6cb9eecffbd8d9f60b3e00b814bbcaa226b89950
Makay131 <mahmutenesakay9@gmail.com>
2024-05-19 17:24:24 +0300

    feat: sb file update for image comp

--- src/components/atoms/image/Image.stories.tsx
+++ src/components/atoms/image/Image.stories.tsx
@@ -4,8 +4,23 @@ import Image from './Image';
 
 const meta: Meta<typeof Image> = {
   component: Image,
+  argTypes: {
+    src: { control: 'text' },
+    alt: { control: 'text' },
+    width: { control: 'text' },
+    height: { control: 'text' },
+  },
   tags: ['autodocs'],
 };
 
 export default meta;
 type Story = StoryObj<typeof Image>;
+
+export const Default: Story = {
+  args: {
+    src: 'https://via.placeholder.com/150',
+    alt: 'Placeholder Image',
+    width: '150px',
+    height: '150px',
+  },
+};
